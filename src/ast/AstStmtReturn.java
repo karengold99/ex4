7205414f8e17b201174f4b2f6ffe5d3ef05a8f48
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 01:23:08 +0200

    Update AstStmtReturn.java

--- src/ast/AstStmtReturn.java
+++ src/ast/AstStmtReturn.java
@@ -1,5 +1,11 @@
 package ast;
 
+import types.*;
+import semantic.SemanticException;
+import symboltable.SymbolTable;
+import temp.*;
+import ir.*;
+
 public class AstStmtReturn extends AstStmt
 {
 	/****************/
@@ -46,5 +52,42 @@ public class AstStmtReturn extends AstStmt
 		/* PRINT Edges to AST GRAPHVIZ DOT file */
 		/****************************************/
 		if (exp != null) AstGraphviz.getInstance().logEdge(serialNumber,exp.serialNumber);
+	}
+
+	@Override
+	public Type semantMe() throws SemanticException
+	{
+		// PDF 2.5: Return statements only inside functions
+		if (!SymbolTable.getInstance().insideFunction())
+			throw new SemanticException(lineNumber, "return statement outside of function");
+		
+		Type expectedReturn = SymbolTable.getInstance().getReturnType();
+
+		if (exp == null) {
+			// return; - must be void function
+			if (!expectedReturn.isVoid())
+				throw new SemanticException(lineNumber, "non-void function must return a value");
+		} else {
+			// return exp; - must match return type
+			Type actualReturn = exp.semantMe();
+			if (expectedReturn.isVoid())
+				throw new SemanticException(lineNumber, "void function cannot return a value");
+			if (!TypeUtils.canAssignTo(actualReturn, expectedReturn))
+				throw new SemanticException(lineNumber, "return type mismatch");
+		}
+
+		return null;
+	}
+
+	@Override
+	public Temp irMe()
+	{
+		if (exp != null) {
+			Temp retTemp = exp.irMe();
+			Ir.getInstance().AddIrCommand(new IrCommandReturn(retTemp));
+		} else {
+			Ir.getInstance().AddIrCommand(new IrCommandReturn(null));
+		}
+		return null;
 	}
 }
