e74424441ea36ba400b726ebbaf3c4828cb4cadb
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 01:13:31 +0200

    Update AstExpParen.java

--- src/ast/AstExpParen.java
+++ src/ast/AstExpParen.java
@@ -1,1 +1,44 @@
+package ast;
+
+import types.*;
+import semantic.SemanticException;
+import temp.*;
+
+public class AstExpParen extends AstExp
+{
+	public AstExp exp;
+
+	public AstExpParen(AstExp exp)
+	{
+		serialNumber = AstNodeSerialNumber.getFresh();
+		this.exp = exp;
+	}
+
+	public void printMe()
+	{
+		System.out.print("AST NODE PAREN EXP\n");
+		if (exp != null) exp.printMe();
+		AstGraphviz.getInstance().logNode(serialNumber, "(EXP)");
+		if (exp != null) AstGraphviz.getInstance().logEdge(serialNumber, exp.serialNumber);
+	}
+
+	@Override
+	public Type semantMe() throws SemanticException
+	{
+		// Just return the type of the inner expression
+		return exp.semantMe();
+	}
+
+	@Override
+	public Integer getConstantValue() {
+		return exp.getConstantValue();
+	}
+
+	@Override
+	public Temp irMe()
+	{
+		// Parentheses don't change semantics, just delegate to inner expression
+		return exp.irMe();
+	}
+}
 
