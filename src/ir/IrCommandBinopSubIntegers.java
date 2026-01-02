18f3ecdbe8ed04c6caf21c5e7f05f3cf32dfa7b3
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 00:40:03 +0200

    Create IrCommandBinopSubIntegers.java

---
+++ src/ir/IrCommandBinopSubIntegers.java
@@ -0,0 +1,39 @@
+/***********/
+/* PACKAGE */
+/***********/
+package ir;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+import temp.*;
+
+public class IrCommandBinopSubIntegers extends IrCommand
+{
+	public Temp t1;
+	public Temp t2;
+	public Temp dst;
+	
+	public IrCommandBinopSubIntegers(Temp dst, Temp t1, Temp t2)
+	{
+		this.dst = dst;
+		this.t1 = t1;
+		this.t2 = t2;
+	}
+	
+	public Temp getDst() { return dst; }
+	public Temp getT1() { return t1; }
+	public Temp getT2() { return t2; }
+	
+	@Override
+	public String toString()
+	{
+		return String.format("Temp_%d := Temp_%d - Temp_%d", 
+			dst.getSerialNumber(), t1.getSerialNumber(), t2.getSerialNumber());
+	}
+}
+
