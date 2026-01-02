e8eb0a85ae099697a00533c0508fb9d26c4d932f
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 00:28:05 +0200

    Update TypeFunction.java

--- src/types/TypeFunction.java
+++ src/types/TypeFunction.java
@@ -2,23 +2,51 @@ package types;
 
 public class TypeFunction extends Type
 {
-	/***********************************/
-	/* The return type of the function */
-	/***********************************/
-	public Type returnType;
+    public Type returnType;
+    public TypeList params;
+    
+    public TypeFunction(Type returnType, String name, TypeList params)
+    {
+        super(name);
+        this.returnType = returnType;
+        this.params = params;
+    }
 
-	/*************************/
-	/* types of input params */
-	/*************************/
-	public TypeList params;
-	
-	/****************/
-	/* CTROR(S) ... */
-	/****************/
-	public TypeFunction(Type returnType, String name, TypeList params)
-	{
-		this.name = name;
-		this.returnType = returnType;
-		this.params = params;
-	}
+    /*==================================================================*/
+    /*                    TYPE CHECK                                    */
+    /*==================================================================*/
+    @Override
+    public boolean isFunction() { return true; }
+
+
+    /** Check if signatures match (for method override) */
+    public boolean signatureMatches(TypeFunction other)
+    {
+        if (this.returnType != other.returnType)
+        {
+            return false;
+        }
+        if (this.paramCount() != other.paramCount())
+        {
+            return false;
+        }
+        TypeList p1 = this.params;
+        TypeList p2 = other.params;
+        while (p1 != null && p2 != null)
+        {
+            if (p1.head != p2.head)
+            {
+                return false;
+            }
+            p1 = p1.tail;
+            p2 = p2.tail;
+        }
+        return true;
+    }
+
+    /** Get number of parameters */
+    public int paramCount()
+    {
+        return params != null ? params.size() : 0;
+    }
 }
