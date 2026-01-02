67beb0297cd8267e6cc7a1c857a740e4b7547015
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 00:40:24 +0200

    Update IRcommandConstInt.java

--- src/ir/IRcommandConstInt.java
+++ src/ir/IRcommandConstInt.java
@@ -22,4 +22,13 @@ public class IRcommandConstInt extends IrCommand
 		this.t = t;
 		this.value = value;
 	}
+	
+	public Temp getDst() { return t; }
+	public int getValue() { return value; }
+	
+	@Override
+	public String toString()
+	{
+		return String.format("Temp_%d := %d", t.getSerialNumber(), value);
+	}
 }
