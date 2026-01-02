f2a19748e92808995b6924a5aa2c2a7f66986913
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 01:05:56 +0200

    Update AstExpCall.java

--- src/ast/AstExpCall.java
+++ src/ast/AstExpCall.java
@@ -1,66 +1,110 @@
 package ast;
 
+import types.*;
+import semantic.SemanticException;
+import symboltable.SymbolTable;
 import temp.*;
 import ir.*;
 
 public class AstExpCall extends AstExp
 {
-	/****************/
-	/* DATA MEMBERS */
-	/****************/
+	public AstExpVar var;      // null for global function call, non-null for method call
 	public String funcName;
-	public AstExpList params;
+	public AstExpList args;
 
-	/******************/
-	/* CONSTRUCTOR(S) */
-	/******************/
-	public AstExpCall(String funcName, AstExpList params)
+	public AstExpCall(AstExpVar var, String funcName, AstExpList args)
 	{
-		/******************************/
-		/* SET A UNIQUE SERIAL NUMBER */
-		/******************************/
 		serialNumber = AstNodeSerialNumber.getFresh();
-
+		this.var = var;
 		this.funcName = funcName;
-		this.params = params;
+		this.args = args;
 	}
 
-	/************************************************/
-	/* The printing message for a call exp AST node */
-	/************************************************/
 	public void printMe()
 	{
-		/********************************/
-		/* AST NODE TYPE = AST CALL EXP */
-		/********************************/
-		System.out.format("CALL(%s)\nWITH:\n",funcName);
-
-		/***************************************/
-		/* RECURSIVELY PRINT params + body ... */
-		/***************************************/
-		if (params != null) params.printMe();
+		System.out.format("CALL(%s)\n", funcName);
+		if (var != null) var.printMe();
+		if (args != null) args.printMe();
 		
-		/***************************************/
-		/* PRINT Node to AST GRAPHVIZ DOT file */
-		/***************************************/
-		AstGraphviz.getInstance().logNode(
-                serialNumber,
-			String.format("CALL(%s)\nWITH",funcName));
-		
-		/****************************************/
-		/* PRINT Edges to AST GRAPHVIZ DOT file */
-		/****************************************/
-		AstGraphviz.getInstance().logEdge(serialNumber,params.serialNumber);
+		AstGraphviz.getInstance().logNode(serialNumber,
+			String.format("CALL\n%s", funcName));
+		if (var != null) AstGraphviz.getInstance().logEdge(serialNumber, var.serialNumber);
+		if (args != null) AstGraphviz.getInstance().logEdge(serialNumber, args.serialNumber);
 	}
 
+	@Override
+	public Type semantMe() throws SemanticException
+	{
+		TypeFunction funcType = null;
+
+		if (var == null) {
+			// Global function call: funcName(args)
+			Type t = SymbolTable.getInstance().find(funcName);
+			if (t == null)
+				throw new SemanticException(lineNumber, "function '" + funcName + "' not found");
+			if (!(t instanceof TypeFunction))
+				throw new SemanticException(lineNumber, "'" + funcName + "' is not a function");
+			funcType = (TypeFunction) t;
+		} else {
+			// Method call: var.funcName(args)
+			Type varType = var.semantMe();
+			if (!varType.isClass())
+				throw new SemanticException(lineNumber, "method call on non-class type");
+			
+			TypeClass classType = (TypeClass) varType;
+			TypeClassVarDec member = classType.findMemberInHierarchy(funcName);
+			if (member == null)
+				throw new SemanticException(lineNumber, "method '" + funcName + "' not found in class");
+			if (!(member.t instanceof TypeFunction))
+				throw new SemanticException(lineNumber, "'" + funcName + "' is not a method");
+			funcType = (TypeFunction) member.t;
+		}
+
+		// Check argument count and types
+		TypeList expectedParams = funcType.params;
+		AstExpList actualArgs = args;
+
+		while (expectedParams != null && actualArgs != null) {
+			Type expectedType = expectedParams.head;
+			Type actualType = actualArgs.head.semantMe();
+			
+			if (!TypeUtils.canAssignTo(actualType, expectedType))
+				throw new SemanticException(lineNumber, "argument type mismatch in call to '" + funcName + "'");
+			
+			expectedParams = expectedParams.tail;
+			actualArgs = actualArgs.tail;
+		}
+
+		// Check same number of arguments
+		if (expectedParams != null || actualArgs != null)
+			throw new SemanticException(lineNumber, "wrong number of arguments in call to '" + funcName + "'");
+
+		return funcType.returnType;
+	}
+
+	@Override
 	public Temp irMe()
 	{
-		Temp t = null;
-
-		if (params != null) { t = params.head.irMe(); }
-
-		Ir.getInstance().AddIrCommand(new IrCommandPrintInt(t));
-
-		return null;
+		// Generate IR for all arguments first
+		AstExpList currArg = args;
+		while (currArg != null) {
+			currArg.head.irMe();
+			currArg = currArg.tail;
+		}
+		
+		// Handle PrintInt specially (library function)
+		if (funcName.equals("PrintInt")) {
+			// Get the first argument temp
+			if (args != null) {
+				Temp argTemp = args.head.irMe();
+				Ir.getInstance().AddIrCommand(new IrCommandPrintInt(argTemp));
+			}
+			return null;  // PrintInt returns void
+		}
+		
+		// For other functions, we would emit a call instruction
+		// For now, return a fresh temp for non-void functions
+		Temp result = TempFactory.getInstance().getFreshTemp();
+		return result;
 	}
 }
