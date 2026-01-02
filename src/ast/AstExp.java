e39a4d95183f788c613ff61b7b0a925aaa783773
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 01:04:15 +0200

    Update AstExp.java

--- src/ast/AstExp.java
+++ src/ast/AstExp.java
@@ -1,14 +1,19 @@
 package ast;
 
 import types.*;
+import semantic.SemanticException;
 
 public abstract class AstExp extends AstNode
 {
-	/***********************************************/
-	/* The default semantic action for an AST node */
-	/***********************************************/
-	public Type semantMe()
-	{
+	@Override
+	public abstract Type semantMe() throws SemanticException;
+
+	/**
+	 * Try to evaluate this expression as a constant integer.
+	 * Returns null if the expression is not a compile-time constant.
+	 * Subclasses should override this method if they can be constant expressions.
+	 */
+	public Integer getConstantValue() {
 		return null;
 	}
 }
