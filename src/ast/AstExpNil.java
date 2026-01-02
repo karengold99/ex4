87c3ec5eb5f4b1da43e47c6c02d7b2305e6d2fc7
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 01:13:05 +0200

    Update AstExpNil.java

--- src/ast/AstExpNil.java
+++ src/ast/AstExpNil.java
@@ -1,1 +1,37 @@
+package ast;
+
+import types.*;
+import semantic.SemanticException;
+import temp.*;
+import ir.*;
+
+public class AstExpNil extends AstExp
+{
+	public AstExpNil()
+	{
+		serialNumber = AstNodeSerialNumber.getFresh();
+		// Debug disabled: 0
+	}
+
+	public void printMe()
+	{
+		System.out.print("AST NODE NIL\n");
+		AstGraphviz.getInstance().logNode(serialNumber, "NIL");
+	}
+
+	@Override
+	public Type semantMe() throws SemanticException
+	{
+		return TypeNil.getInstance();
+	}
+
+	@Override
+	public Temp irMe()
+	{
+		// Nil is represented as 0 (null pointer)
+		Temp t = TempFactory.getInstance().getFreshTemp();
+		Ir.getInstance().AddIrCommand(new IRcommandConstInt(t, 0));
+		return t;
+	}
+}
 
