8c5155001e38dc66d4ff6e33e27b7899af8a17d7
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2026-01-03 01:23:25 +0200

    Update AstStmtWhile.java

--- src/ast/AstStmtWhile.java
+++ src/ast/AstStmtWhile.java
@@ -1,5 +1,8 @@
 package ast;
 
+import types.*;
+import semantic.SemanticException;
+import symboltable.SymbolTable;
 import temp.*;
 import ir.*;
 
@@ -13,59 +16,67 @@ public class AstStmtWhile extends AstStmt
 	/*******************/
 	public AstStmtWhile(AstExp cond, AstStmtList body)
 	{
+		serialNumber = AstNodeSerialNumber.getFresh();
 		this.cond = cond;
 		this.body = body;
 	}
 
+	public void printMe()
+	{
+		System.out.print("AST NODE STMT WHILE\n");
+		if (cond != null) cond.printMe();
+		if (body != null) body.printMe();
+
+		AstGraphviz.getInstance().logNode(serialNumber, "WHILE");
+		if (cond != null) AstGraphviz.getInstance().logEdge(serialNumber, cond.serialNumber);
+		if (body != null) AstGraphviz.getInstance().logEdge(serialNumber, body.serialNumber);
+	}
+
+	@Override
+	public Type semantMe() throws SemanticException
+	{
+		// PDF 2.5: condition must be int
+		Type condType = cond.semantMe();
+		if (!condType.isInt())
+			throw new SemanticException(cond.lineNumber, "while condition must be int");
+
+		// Begin scope for while body
+		SymbolTable.getInstance().beginScope();
+
+		if (body != null)
+			body.semantMe();
+
+		SymbolTable.getInstance().endScope();
+
+		return null;
+	}
+
+	@Override
 	public Temp irMe()
 	{
-		/*******************************/
-		/* [1] Allocate 2 fresh labels */
-		/*******************************/
-		String labelEnd   = IrCommand.getFreshLabel("end");
-		String labelStart = IrCommand.getFreshLabel("start");
-
-		/*********************************/
-		/* [2] entry label for the while */
-		/*********************************/
-		Ir.
-				getInstance().
-				AddIrCommand(new IrCommandLabel(labelStart));
-
-		/********************/
-		/* [3] cond.IRme(); */
-		/********************/
+		String beginLabel = IrCommand.getFreshLabel("while_begin");
+		String afterLabel = IrCommand.getFreshLabel("while_after");
+		
+		// Emit begin label
+		Ir.getInstance().AddIrCommand(new IrCommandLabel(beginLabel));
+		
+		// Generate condition
 		Temp condTemp = cond.irMe();
-
-		/******************************************/
-		/* [4] Jump conditionally to the loop end */
-		/******************************************/
-		Ir.
-				getInstance().
-				AddIrCommand(new IrCommandJumpIfEqToZero(condTemp,labelEnd));
-
-		/*******************/
-		/* [5] body.IRme() */
-		/*******************/
-		body.irMe();
-
-		/******************************/
-		/* [6] Jump to the loop entry */
-		/******************************/
-		Ir.
-				getInstance().
-				AddIrCommand(new IrCommandJumpLabel(labelStart));
-
-		/**********************/
-		/* [7] Loop end label */
-		/**********************/
-		Ir.
-				getInstance().
-				AddIrCommand(new IrCommandLabel(labelEnd));
-
-		/*******************/
-		/* [8] return null */
-		/*******************/
+		
+		// If false (0), exit loop
+		Ir.getInstance().AddIrCommand(new IrCommandJumpIfEqToZero(condTemp, afterLabel));
+		
+		// Generate body
+		if (body != null) {
+			body.irMe();
+		}
+		
+		// Jump back to begin
+		Ir.getInstance().AddIrCommand(new IrCommandJumpLabel(beginLabel));
+		
+		// Emit after label
+		Ir.getInstance().AddIrCommand(new IrCommandLabel(afterLabel));
+		
 		return null;
 	}
 }
